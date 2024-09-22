017ae3bb39cc50fe7fe9023e3ae754577c6423c0
abc <eren.sistemmep@gmail.com>
2024-09-22 17:27:07 +0300

    first commit

---
+++ helloword.js
@@ -0,0 +1,1 @@
+//console.log('aecloud script external')
---
+++ script.js
@@ -0,0 +1,11 @@
+//console.log('aecloud script external')
+eren = 45 
+aecloud = 45
+console.log(typeof eren+ typeof aecloud ,":", aecloud+eren)
+console.log (typeof 45)
+console.log (typeof eren)
+console.log (typeof true)
+console.log (typeof null)
+console.log (typeof undefined)
+
+// yorum satırları 
