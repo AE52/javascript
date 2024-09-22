42b4901a3c777dc51c3211d5c7f6ec581c3b68fb
abc <eren.sistemmep@gmail.com>
2024-09-22 17:33:24 +0300

    1. video

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ console.log (typeof null)
 console.log (typeof undefined)
 
 // yorum satırları 
+
+let a = 3
+let b = 4
+let c = a + b
+console.log(c)
+
+const erenca = 45
+console.log(erenca)
+
+const $eren = 54
+console.log($eren)
